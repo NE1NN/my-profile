c971b2ff27f3271be53aa95c5256e1b25863fbbe
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-05-25 00:25:41 +1000

    Add sections

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -3,24 +3,27 @@ import profile from './profile.png'
 function MainContent() {
   return (
     <main>
-      <img src={profile}></img>
-      <h1>HI I&apos;M HINDIE!</h1>
-      <section>
-        <h3>ABOUT ME</h3>
-        <ul>
-          <li>Name: Hindie Suputra</li>
-          <li>Age: 19</li>
-          <li>Degree: Computer Science</li>
-        </ul>
+      <section className='main-profile'>
+        <img src={profile}></img>
+        <h1>HI I&apos;M HINDIE!</h1>
       </section>
-      <section>
+      <section className='about-me'>
+        <h3>ABOUT ME</h3>
+        <div className='elem'>Name: Hindie Suputra</div>
+        <div className='elem'>Age: 19</div>
+        <div className='elem'>Degree: Computer Science</div>
+      </section>
+      <section className='hobbies'>
         <h3>HOBBIES</h3>
-        <ul>
-          <li>Guitar</li>
-          <li>Volleyball</li>
-          <li>Gym</li>
-          <li>Video Games</li>
-        </ul>
+        <div className='elem'>Guitar</div>
+        <div className='elem'>Volleyball</div>
+        <div className='elem'>Gym</div>
+        <div className='elem'>Video Games</div>
+      </section>
+      <section className='projects'>
+        <h3>PROJECTS</h3>
+        <div className='elem'>Valorant Tracker Bot</div>
+        <div className='elem'>Gym Progress Tracker</div>
       </section>
     </main>
   )
